01449ab724eff94c133a258acffb1f74bf469d4d
Christopher Ta <39348633+Digital1O1@users.noreply.github.com>
2021-07-16 15:30:28 -0500

    Delete UART_Hello_World directory

--- UART_Hello_World/build/default/production/mcc_generated_files/drivers/uart.o.d
+++
@@ -1,7 +0,0 @@
-build/default/production/mcc_generated_files/drivers/uart.o: \
- mcc_generated_files/drivers/uart.c mcc_generated_files/drivers/uart.h \
- mcc_generated_files/drivers/../uart2.h
-
-mcc_generated_files/drivers/uart.h:
-
-mcc_generated_files/drivers/../uart2.h:
--- UART_Hello_World/build/default/production/mcc_generated_files/uart2.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/mcc_generated_files/uart2.o: \
- mcc_generated_files/uart2.c mcc_generated_files/uart2.h
-
-mcc_generated_files/uart2.h:
